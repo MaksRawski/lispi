474211a05a7c8a61c8b3dae5c9042b39e97ea157
Maks Rawski <maksymilian.rawski@tutanota.com>
2023-10-07 01:18:27 +0200

    update main to work with new eval

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 mod repl;
-use lispi::parser::parse;
+use lispi::{
+    interpreter::eval,
+    parser::parse,
+    types::{NullableList, NIL},
+};
 use repl::MyHelper;
-use rustyline::{error::ReadlineError, Cmd, Editor};
+use rustyline::{error::ReadlineError, Editor};
 
 fn main() {
     env_logger::builder()
@@ -9,6 +13,7 @@ fn main() {
         .format_target(false)
         .init();
     let mut rl = Editor::new().unwrap();
+    let mut a: NullableList = NIL.into();
     rl.set_helper(Some(MyHelper::new()));
 
     loop {
@@ -17,8 +22,9 @@ fn main() {
             Ok(line) => {
                 rl.add_history_entry(line.as_str()).unwrap();
                 match parse(&line) {
-                    Some(prog) => match prog.eval() {
-                        Some(result) => {
+                    Some(prog) => match eval(prog, a.clone()) {
+                        Some((result, new_a_list)) => {
+                            a = new_a_list;
                             println!("{}", result)
                         }
                         None => {
