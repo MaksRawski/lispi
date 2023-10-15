0d516e1edf3b48603c9cc60ec2855162e54d9d05
Maks Rawski <maksymilian.rawski@tutanota.com>
2023-10-15 22:15:21 +0200

    simplify bracket highlighting

--- src/repl.rs
+++ src/repl.rs
@@ -1,5 +1,4 @@
 use std::borrow::Cow::{self, Borrowed, Owned};
-use std::cell::Cell;
 
 use rustyline::hint::HistoryHinter;
 use rustyline::{
@@ -21,67 +20,64 @@ impl MyHelper {
     pub fn new() -> Self {
         Self {
             brackets: MatchingBracketValidator::new(),
-            highlighter: MyMatchingBracketHighlighter::new(),
+            highlighter: MyMatchingBracketHighlighter {},
             hinter: HistoryHinter {},
         }
     }
 }
 
-struct MyMatchingBracketHighlighter {
-    bracket: Cell<Option<(char, usize)>>,
-}
+struct MyMatchingBracketHighlighter;
 
-impl MyMatchingBracketHighlighter {
-    pub fn new() -> Self {
-        Self {
-            bracket: Cell::new(None),
-        }
+#[inline]
+fn other_bracket(bracket: char) -> char {
+    if bracket == '(' {
+        ')'
+    } else {
+        '('
     }
 }
 
 impl Highlighter for MyMatchingBracketHighlighter {
-    fn highlight<'l>(&self, line: &'l str, _pos: usize) -> Cow<'l, str> {
-        match self.bracket.get() {
-            Some((c, idx)) => match find_matching_bracket(line, idx) {
-                Ok((match_c, match_idx)) => {
-                    let mut line = line.to_owned();
-                    if idx > match_idx {
-                        line.replace_range(idx..=idx, &format!("\x1b[1;34m{}\x1b[0m", c));
-                        line.replace_range(
-                            match_idx..=match_idx,
-                            &format!("\x1b[1;34m{}\x1b[0m", match_c),
-                        );
-                    } else {
-                        line.replace_range(
-                            match_idx..=match_idx,
-                            &format!("\x1b[1;34m{}\x1b[0m", match_c),
-                        );
-                        line.replace_range(idx..=idx, &format!("\x1b[1;34m{}\x1b[0m", c));
-                    }
-                    Owned(line)
+    fn highlight<'l>(&self, line: &'l str, pos: usize) -> Cow<'l, str> {
+        let current_char = if let Some(c) = line.chars().nth(pos) {
+            c
+        } else {
+            return Borrowed(line);
+        };
+
+        match find_matching_bracket(line, pos) {
+            Ok(match_pos) => {
+                let mut line = line.to_owned();
+                if pos > match_pos {
+                    line.replace_range(pos..=pos, &format!("\x1b[1;34m{}\x1b[0m", current_char));
+                    line.replace_range(
+                        match_pos..=match_pos,
+                        &format!("\x1b[1;34m{}\x1b[0m", other_bracket(current_char)),
+                    );
+                } else {
+                    line.replace_range(
+                        match_pos..=match_pos,
+                        &format!("\x1b[1;34m{}\x1b[0m", other_bracket(current_char)),
+                    );
+                    line.replace_range(pos..=pos, &format!("\x1b[1;34m{}\x1b[0m", current_char));
                 }
-                Err(true) => {
-                    let mut line = line.to_owned();
-                    line.replace_range(idx..=idx, &format!("\x1b[1;31m{}\x1b[0m", c));
-                    Owned(line)
-                }
-                Err(false) => Borrowed(line),
-            },
-            None => Borrowed(line),
+                Owned(line)
+            }
+            Err(true) => {
+                let mut line = line.to_owned();
+                line.replace_range(pos..=pos, &format!("\x1b[1;31m{}\x1b[0m", current_char));
+                Owned(line)
+            }
+            Err(false) => Borrowed(line),
         }
     }
 
     fn highlight_char(&self, line: &str, pos: usize) -> bool {
         if let Some(c) = line.chars().nth(pos) {
-            if c == '(' || c == ')' {
-                self.bracket.set(Some((c, pos)));
-            } else {
-                self.bracket.set(None);
-            }
+            c == '(' || c == ')'
         } else {
-            self.bracket.set(None);
+            false
         }
-        self.bracket.get().is_some()
     }
 }
 
@@ -91,7 +87,7 @@ impl Highlighter for MyMatchingBracketHighlighter {
 /// If the cursor is on a bracket but there was no match, it will return `Err(true)`.
 /// If the cursor is on a bracket and it finds a matching bracket, it will return `Ok(usize)`
 /// with the index of the matching bracket.
-fn find_matching_bracket(line: &str, cursor_pos: usize) -> Result<(char, usize), bool> {
+pub(crate) fn find_matching_bracket(line: &str, cursor_pos: usize) -> Result<usize, bool> {
     let mut depth = 0;
     match line.chars().nth(cursor_pos).ok_or(false)? {
         '(' => {
@@ -100,7 +96,7 @@ fn find_matching_bracket(line: &str, cursor_pos: usize) -> Result<(char, usize),
                     depth += 1;
                 } else if c == ')' {
                     if depth == 0 {
-                        return Ok((c, cursor_pos + 1 + i));
+                        return Ok(cursor_pos + 1 + i);
                     } else {
                         depth -= 1;
                     }
@@ -114,7 +110,7 @@ fn find_matching_bracket(line: &str, cursor_pos: usize) -> Result<(char, usize),
                     depth += 1;
                 } else if c == '(' {
                     if depth == 0 {
-                        return Ok((c, cursor_pos - i - 1));
+                        return Ok(cursor_pos - i - 1);
                     } else {
                         depth -= 1;
                     }
@@ -128,12 +124,12 @@ fn find_matching_bracket(line: &str, cursor_pos: usize) -> Result<(char, usize),
 
 #[test]
 fn test_find_matching_bracket() {
-    assert_eq!(find_matching_bracket("(()())", 0), Ok((')', 5)));
-    assert_eq!(find_matching_bracket("(()())", 1), Ok((')', 2)));
-    assert_eq!(find_matching_bracket("(()())", 2), Ok(('(', 1)));
-    assert_eq!(find_matching_bracket("(()())", 3), Ok((')', 4)));
-    assert_eq!(find_matching_bracket("(()())", 4), Ok(('(', 3)));
-    assert_eq!(find_matching_bracket("(()())", 5), Ok(('(', 0)));
+    assert_eq!(find_matching_bracket("(()())", 0), Ok(5));
+    assert_eq!(find_matching_bracket("(()())", 1), Ok(2));
+    assert_eq!(find_matching_bracket("(()())", 2), Ok(1));
+    assert_eq!(find_matching_bracket("(()())", 3), Ok(4));
+    assert_eq!(find_matching_bracket("(()())", 4), Ok(3));
+    assert_eq!(find_matching_bracket("(()())", 5), Ok(0));
 
     assert_eq!(find_matching_bracket("(ABC", 0), Err(true));
     assert_eq!(find_matching_bracket("ABC)", 3), Err(true));
