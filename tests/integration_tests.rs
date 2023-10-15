4623eb83afb3cf804210b4fa034b37a3a47304a5
Maks Rawski <maksymilian.rawski@tutanota.com>
2023-10-16 00:29:06 +0200

    complicate the peano fac test
    return a list of first 5 factorials!

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,9 +1,14 @@
 use lispi::{file_parser::eval_file, types::NIL};
 
 #[test]
-fn test_eval_file() {
+fn test_eval_file_peano_fac_list() {
     let mut output = Vec::with_capacity(10);
-    let expected_output = format!("{}{}", "NIL\n".repeat(7), "24\n");
+    let expected_output = format!(
+        "{}{}\n{}\n",
+        "NIL\n".repeat(10),
+        "[0, 1, 2, 3, 4]",
+        "[1, 1, 2, 6, 24]"
+    );
     assert_eq!(
         eval_file("peano.lisp", &mut NIL.into(), &mut output),
         Some(())
